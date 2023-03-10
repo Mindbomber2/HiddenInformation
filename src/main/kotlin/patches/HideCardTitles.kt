19daf57cceac874cb8cfbaf30f1f810042e9eb8f
Kiooeht <kiooeht@gmail.com>
2023-03-10 00:01:11 -0800

    Fix crash when toggling card titles sometimes

--- src/main/kotlin/patches/HideCardTitles.kt
+++ src/main/kotlin/patches/HideCardTitles.kt
@@ -1,11 +1,14 @@
 package com.evacipated.cardcrawl.mod.hiddeninfo.patches
 
 import com.evacipated.cardcrawl.mod.hiddeninfo.HiddenConfig
+import com.evacipated.cardcrawl.mod.hiddeninfo.extensions.iz
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatches2
-import com.evacipated.cardcrawl.modthespire.lib.SpireReturn
 import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.helpers.FontHelper
 import com.megacrit.cardcrawl.screens.SingleCardViewPopup
+import javassist.expr.ExprEditor
+import javassist.expr.MethodCall
 
 @SpirePatches2(
     SpirePatch2(
@@ -19,10 +22,20 @@ import com.megacrit.cardcrawl.screens.SingleCardViewPopup
 )
 object HideCardTitles {
     @JvmStatic
-    fun Prefix(): SpireReturn<Void> {
-        if (HiddenConfig.cardTitles) {
-            return SpireReturn.Return()
+    fun Instrument(): ExprEditor =
+        object : ExprEditor() {
+            override fun edit(m: MethodCall) {
+                if (m.iz(FontHelper::class, "renderRotatedText") || m.iz(FontHelper::class, "renderFontCentered")) {
+                    m.replace(
+                        "if (!${HideCardTitles::class.qualifiedName}.hide()) {" +
+                                "\$_ = \$proceed(\$\$);" +
+                                "}"
+                    )
+                }
+            }
         }
-        return SpireReturn.Continue()
-    }
+
+    @JvmStatic
+    fun hide(): Boolean =
+        HiddenConfig.cardTitles
 }
