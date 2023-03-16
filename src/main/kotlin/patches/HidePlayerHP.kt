9f0c881ae48151519f58f9154c21eb5769ae2e1d
Kiooeht <kiooeht@gmail.com>
2023-03-15 18:42:03 -0700

    Hide heal amount when hovering rest option at campfire

--- src/main/kotlin/patches/HidePlayerHP.kt
+++ src/main/kotlin/patches/HidePlayerHP.kt
@@ -7,8 +7,11 @@ import com.evacipated.cardcrawl.modthespire.lib.SpireReturn
 import com.megacrit.cardcrawl.characters.AbstractPlayer
 import com.megacrit.cardcrawl.core.AbstractCreature
 import com.megacrit.cardcrawl.helpers.FontHelper
+import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption
+import com.megacrit.cardcrawl.ui.campfire.RestOption
 import com.megacrit.cardcrawl.ui.panels.TopPanel
 import javassist.expr.ExprEditor
+import javassist.expr.FieldAccess
 import javassist.expr.MethodCall
 
 object HidePlayerHP {
@@ -37,16 +40,44 @@ object HidePlayerHP {
                 override fun edit(m: MethodCall) {
                     if (m.iz(FontHelper::class, "renderFontLeftTopAligned")) {
                         m.replace(
-                            "if (!${TopPanelHP::class.qualifiedName}.hide()) {" +
+                            "if (!${HidePlayerHP::class.qualifiedName}.hide()) {" +
                                     "\$_ = \$proceed(\$\$);" +
                                     "}"
                         )
                     }
                 }
             }
+    }
+
+    @SpirePatch2(
+        clz = AbstractCampfireOption::class,
+        method = "render"
+    )
+    object CampfireRestOption {
+        @JvmStatic
+        fun Instrument(): ExprEditor =
+            object : ExprEditor() {
+                override fun edit(f: FieldAccess) {
+                    if (f.iz(AbstractCampfireOption::class, "description") && f.isReader) {
+                        f.replace(
+                            "\$_ = \$proceed(\$\$);" +
+                                    "if (this instanceof ${RestOption::class.qualifiedName} && ${HidePlayerHP::class.qualifiedName}.hide()) {" +
+                                    "\$_ = ${CampfireRestOption::class.qualifiedName}.filter(\$_);" +
+                                    "}"
+                        )
+                    }
+                }
+            }
+
+        private val regex = Regex(""" ?\(\d+\)""")
 
         @JvmStatic
-        fun hide(): Boolean =
-            HiddenConfig.playerHP
+        fun filter(description: String): String {
+            return regex.replace(description, "")
+        }
     }
+
+    @JvmStatic
+    fun hide(): Boolean =
+        HiddenConfig.playerHP
 }
